9466ad93acb855f90c1a5ffd9e1de189027aa9eb
root <root@docker01.home.dihei.ch>
2014-09-12 01:09:08 +0200

    add support of java and jenkins env variables for settings like timezone

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-exec sudo -u jenkins -H java -jar /usr/share/jenkins/jenkins.war
+exec sudo -u jenkins -H -E bash -c "/usr/bin/java $JAVA_ARGS $JENKINS_JAVA_OPTIONS $JENKINS_OPTS -jar /usr/share/jenkins/jenkins.war"
+
